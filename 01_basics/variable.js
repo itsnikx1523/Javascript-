1b7c854b5eafc3bf0316b85f563b0fa95a60465e
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-03-28 15:13:14 +0000

    config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const account= 13334567
+let accountEmail = "nikhilthebug2315@gmail.com"
+var accountPassword= "12345@nikhil"
+accountCity= "Hazaribag"
+// account= 2 //not allowed
+
+accountEmail = "mrnikx23@gmail.com"
+accountPassword= "2345#nikx"
+accountCity = "Koderma"
+// console.log(account)
+console.table([account ,accountEmail,accountPassword ,accountCity])
+
+/*
+prefer not to use var because
+of block of scope*/
