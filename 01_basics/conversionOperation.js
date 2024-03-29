9d34247dc0d740079974dee54f6e4f7d7f3e7d5d
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-03-29 07:27:41 +0000

    coversion operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+// let score="33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+// " 33" => 33
+// "33abc" => NaN
+//true =>1 ; false=>0
+
+//let isLoggedIn = 1
+let isLoggedIn= ''
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// '' => false
+// " nikhil "=> true
+
+let someNumber = 1523
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
