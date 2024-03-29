0a8a3fad8a282056b09869eb74d67f5b4751f2c8
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-03-29 06:38:56 +0000

    Datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+  //alert ( 3 + 3)// we are using nodejs,not browser 
+  let name="Nikx_the_bug"
+  let loggedIn= "false"
+  
+
+  //number 2 to power 53
+
+  // bigint 
+  //string => " "
+  //boolean
+  //null => standalone value
+  //undefined =>
+  //symbol
+
+
+  //object
+  //typeof
+
+  console.log(typeof null);
+  console.log(typeof undefined);
