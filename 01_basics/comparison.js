a455ff9304eb32e55decaffef32cb59444c556c4
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-03-31 12:29:24 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,16 @@
+console.log("2" >1);
+console.log("02" > 1);
+
+// In most of the cases avoid this way of doing coding in Js
+
+console.log(null> 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined >  0);
+console.log(undefined < 0)
+
+
+// ===
+console.log("2" === 2);
+console.log("2" == true);
--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -40,3 +40,20 @@ console.log("1" + 2);
 console.log(1+ "2");
 console.log('1'+'2'+3+ 4);
 console.log(1+2 +'2');
+
+console.log(+true);
+console.log(+"");
+
+
+// Increment and Decrement Operator
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
