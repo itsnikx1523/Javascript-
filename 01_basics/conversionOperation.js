199adb6b9f842398d55358c5968e2019704f54af
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-03-31 12:06:32 +0000

    string changes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -20,7 +20,23 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 1523
 
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// *************** Operations ***************
+
+let value =  3
+let negValue = -value 
+console.log(negValue);
+
+
+let str1=" Nikhil"
+let str2="    Singh"
+let str3= str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log('1'+'2'+3+ 4);
+console.log(1+2 +'2');
