35c4253591f454d5fa8a8ab788fdd0077f7581ab
itsnikx1523 <141251180+itsnikx1523@users.noreply.github.com>
2024-04-01 09:21:29 +0000

    summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,21 @@
+//Stack (Primitive) , Heap ( Non -Primitive)
+let myYoutubename = "nikhil"
+
+let anothername = myYoutubename
+anothername= "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne =
+{
+    email:"userOne@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "nikx@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
